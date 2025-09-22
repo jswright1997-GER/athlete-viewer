b122c93db0c0ec7bc60d9756f9713963127b2500
wright <wright@simi.com>
2025-09-22 21:07:29 +0200

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,15 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body style={{ background: "#0b1020", color: "#e2e8f0" }}>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://www.youtube-nocookie.com" />
+        <link rel="preconnect" href="https://i.ytimg.com" />
+        <link rel="dns-prefetch" href="https://www.youtube-nocookie.com" />
+        <link rel="dns-prefetch" href="https://i.ytimg.com" />
+      </head>
+      <body style={{ background: "#0b1020", color: "#e2e8f0" }}>
+        {children}
+      </body>
     </html>
   );
 }
